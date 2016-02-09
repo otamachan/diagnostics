b2ec195744f1d8b7bdca3f2c19e730f79147067c
西野 環 <tamaki_nishino@mail.toyota.co.jp>
2016-02-09 20:49:52 +0900

    wip

--- diagnostic_analysis/src/diagnostic_analysis/exporter.py
+++ diagnostic_analysis/src/diagnostic_analysis/exporter.py
@@ -56,7 +56,7 @@ class LogExporter:
         if self.output_dir is None:
             self.output_dir = tempfile.mkdtemp()
             self._temp = True
-        
+
         if not os.path.isdir(self.output_dir):
             os.makedirs(self.output_dir)
 
@@ -67,7 +67,7 @@ class LogExporter:
             os.remove(file)
         if self._temp:
             os.rmdir(self.output_dir)
-        
+
     ##\brief Return filename of output
     ##\param name str : DiagnosticStatus name ex: 'Mechanism Control'
     def get_filename(self, name):
@@ -83,24 +83,24 @@ class LogExporter:
 
     ##\brief Creates and updates data files with new messages
     def _update(self, topic, msg):
-        if (not (topic == '/diagnostics')):
+        if (not (msg._type == 'diagnostic_msgs/DiagnosticArray')):
             print "Discarding message on topic: %s" % topic
             return
-        
+
         t = time.localtime(float(str(msg.header.stamp)) / 1000000000.0)
-        
+
         for status in msg.status:
             name = status.name
 
             if (not self._stats.has_key(name)):
                 self._stats[name] = {}
-                
+
                 fields = {}
                 for index, s in enumerate(status.values):
                     fields[s.key] = index
-                    
+
                 self._stats[name]['fields'] = fields
-                
+
                 self._stats[name]['level'] = status.level
                 self._stats[name]['message'] = status.message
                 self._stats[name]['hardware_id'] = status.hardware_id
@@ -109,7 +109,7 @@ class LogExporter:
                 datafile, tmp_name = tempfile.mkstemp()
                 self._stats[name]['data_file'] = os.fdopen(datafile, 'w')
                 self._stats[name]['data_name'] = tmp_name
-                
+
 
             # Check to see if fields have changed. Add new fields to map
             if (not [s.key for s in status.values] == self._stats[name]['fields'].keys()):
@@ -118,18 +118,18 @@ class LogExporter:
                         self._stats[name]['fields'][s.key] = len(self._stats[name]['fields'])
 
             # Add values in correct place for header index
-            # Key/Value pairs can move around, this makes sure values are 
+            # Key/Value pairs can move around, this makes sure values are
             # added to correct keys
             vals = []
             for key, val in self._stats[name]['fields'].iteritems():
                 vals.append('')
             for s in status.values:
                 vals[self._stats[name]['fields'][s.key]] = s.value.replace('\n','  ').replace(',',' ')
-        
+
             msg = status.message.replace(',',' ').strip()
             hw_id = status.hardware_id.replace(',', ' ')
-        
-            self._stats[name]['data_file'].write(','.join([time.strftime("%Y/%m/%d %H:%M:%S", t)] + 
+
+            self._stats[name]['data_file'].write(','.join([time.strftime("%Y/%m/%d %H:%M:%S", t)] +
                                             [str(status.level), msg, hw_id] + vals) + '\n')
 
     ##\brief Close logfile, append data to header
@@ -138,23 +138,22 @@ class LogExporter:
             # Sort fields by correct index, add to header
             field_dict = sorted(self._stats[name]['fields'].iteritems(), key=operator.itemgetter(1))
             fields = map(operator.itemgetter(0), field_dict)
-            
-            header_line = ','.join(['Timestamp'] + ['Level', 'Message', 'Hardware ID'] + 
+
+            header_line = ','.join(['Timestamp'] + ['Level', 'Message', 'Hardware ID'] +
                                     [f.replace(',','').replace('\n', ' ') for f in fields]) + '\n'
-            
+
             file_name = os.path.join(self.output_dir, name.replace(' ', '_').replace('(', '').replace(')', '').replace('/', '__').replace('.', '').replace('#', '') + '.csv')
-            
+
             output_file = file(file_name, 'w')
             output_file.write(header_line)
             output_file.close()
 
 
             self._stats[name]['data_file'].close() # Close data
-            
+
             # Append the tmp data file to the header
             subprocess.call("cat %s >> %s" % (self._stats[name]['data_name'], file_name), shell=True)
             # Remove tmp file
             os.remove(self._stats[name]['data_name'])
             # Store filename
             self._stats[name]['file_name'] = file_name
-    
